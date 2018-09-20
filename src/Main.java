e2c62918db2bbaab02f7bf375acd56578577db89
Zander Labuschagne <zander.labuschagne@protonmail.ch>
2018-09-20 02:22:57 +0200

    Created Secure Sign In v3a
    java cli version

---
+++ src/Main.java
@@ -0,0 +1,203 @@
+import java.io.Console; //Input from console, espesially password input
+import java.awt.Toolkit; //Copy function
+import java.awt.datatransfer.Clipboard; //Copy function
+import java.awt.datatransfer.StringSelection; //Copy function
+
+/**
+ * @author Zander Labuschagne <zander.labuschagne@protonmail.ch>
+**/
+
+public class Main
+{
+	private static char option1 = '.';
+	private static char option2 = '.';
+	
+	private static char[] prompt(String prompt)
+	{
+		Console console = System.console();
+		
+		return console.readPassword(prompt + ": ");
+	}
+	
+	private static void argParsePanic(String arg)
+	{
+		System.out.println("Argument \"" + arg + "\" is not defined in Secure Sign In v3.x.x");
+		System.out.println("Try 'ssi --help' for more information.");
+		System.exit(-1);
+	}
+	
+	private static void applicationPanic()
+	{
+		System.out.println("Terminating application due to user disobedience.");
+		System.out.println("Try 'ssi --help' for more information.");
+		System.exit(-1);
+	}
+	
+	private static void printHelp()
+	{
+		System.out.println("Secure Sign In v3.0.0a ( https://gitlab.com/Zander-Labuschagne/SecureSignIn-v3a )");
+		
+		System.out.println("");
+		System.out.println("Usage: ssi [OPTIONS]...");
+		System.out.println("\t-l, --long\t\tDefault long version of the password");
+		System.out.println("\t-s, --short\t\tShort version of the password");
+		System.out.println("\t-c, --copy\t\tCopies the password to memory for 8 seconds");
+		System.out.println("\t-v, --verbose\t\tDisplays the password on the console screen if needed to type over manually");
+		System.out.println("\t\t\t\t(not safe -- only use when absolutely necessary)");
+		System.out.println("\t-h, --help\t\tDisplays this help menu");
+		
+		System.out.println("");
+		System.out.println("EXAMPLES:");
+		System.out.println("\tssi -lc");
+		System.out.println("\tssi -cl");
+		System.out.println("\tssi -lv");
+		System.out.println("\tssi -l -c");
+		System.out.println("\tssi -v -l");
+		System.out.println("ssi -c");
+		System.out.println("ssi --short --copy");
+	}
+	
+	private static void parseArgs(String[] args)
+	{
+		for (short arg = 0; arg < args.length; ++arg) {
+			if (args[arg].length() < 2)
+				argParsePanic(args[arg]);
+			if (args[arg].length() > 2 && args[arg].charAt(0) == '-' && args[arg].charAt(1) == '-') {
+				if (args[arg].equals("--long"))
+					option1 = 'l';
+				else if (args[arg].equals("--short"))
+					option1 = 's';
+				else if (args[arg].equals("--copy"))
+					option2 = 'c';
+				else if (args[arg].equals("--verbose"))
+					option2 = 'v';
+				else if (args[arg].equals("--help"))
+					printHelp();
+				else
+					argParsePanic(args[arg]);
+					
+			} else if (args[arg].length() >= 2 && args[arg].charAt(0) == '-') {
+				for (short i = 1; i < args[arg].length(); ++i) {
+					switch (args[arg].charAt(i)) {
+						case 'l':
+							option1 = 'l';
+							break;
+						case 's':
+							option1 = 's';
+							break;
+						case 'c':
+							option2 = 'c';
+							break;
+						case 'v':
+							option2 = 'v';
+							break;
+						case 'h':
+							printHelp();
+						default:
+							argParsePanic(args[arg]);
+					}
+				}
+			} else 
+				argParsePanic(args[arg]);
+		}
+	}
+	
+	private static void copyCipher(char[] cipher)
+	{
+		Thread deleteCipher = new Thread() {
+			public void run() {
+				try {
+					Thread.sleep(8000);
+					Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+					clipboard.setContents(new StringSelection(""), null);
+				}
+				catch(InterruptedException iex) {
+					System.out.println(iex.getMessage());
+				}
+			}
+		};
+		StringSelection stringSelection = new StringSelection(new String(cipher));
+		Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
+		clpbrd.setContents(stringSelection, null);
+		deleteCipher.start();
+	}
+	
+	private static void printCipher(char[] cipher)
+	{
+		for (short ii = 0; cipher[ii] != '\0'; ++ii)
+			System.out.print(cipher[ii]);
+	}
+	
+	public static void main(String[] args)
+	{
+		try
+		{
+			Console console = System.console();
+			
+			char[] plain;
+			while (true) {
+				plain = prompt("Password");
+				if (new String(plain).length() < 3)
+					System.out.println("Please Enter a Password that contains at least 3 characters");
+				else
+					break;
+			}
+			
+			char[] k;
+			while (true) {
+				k = prompt("Key");
+				if (new String(k).length() < 3)
+					System.out.println("Please Enter a Key that contains at least 3 characters");
+				else
+					break;
+			}
+			
+			parseArgs(args);
+			
+			if (option1 == '.') {
+				System.out.println("Enter 's' to use the shortened version of the password.");
+				System.out.println("Enter 'l' to use the long version of the password. (Default)");
+				System.out.println("Enter 'q' to exit the application.");
+				option1 = console.readLine().charAt(0);
+			}
+			
+			char[] cipher = null;
+			switch (option1) {
+				case 's':
+					cipher = SecureSignIn.encrypt(plain, k, 12);
+					break;
+				case 'l':
+					cipher = SecureSignIn.encrypt(plain, k, 32);
+					break;
+				case 'q':
+					System.exit(0);
+				default:
+					applicationPanic();
+			}
+			
+			if(cipher == null)
+				throw new Exception("Error Occurred During Encryption");
+			
+			if (option2 == '.') {
+				System.out.println("Enter 'c' to copy the password and exit the application.");
+				System.out.println("Enter 'v' to view/display the password on screen and exit the application.");
+				System.out.println("Enter 'q' to exit the application.");
+				option2 = console.readLine().charAt(0);
+			}
+			
+			switch (option2) {
+				case 'c':
+					copyCipher(cipher);
+					System.exit(0);
+				case 'v':
+					printCipher(cipher);
+					System.exit(0);
+				default:
+					applicationPanic();
+			}
+		} catch (Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+	}
+}
---
+++ src/SecureSignIn.java
@@ -0,0 +1,158 @@
+import java.io.Console; //Input from console
+import java.util.LinkedList; //Linked lists
+
+/**
+ * @author Zander Labuschagne
+ * E-Mail: <zander.labuschagne@protonmail.ch>
+ */
+
+public class SecureSignIn
+{
+//        //Instance Variables
+//        private char[] plainPassword;
+//        private char[] key;
+//        private char[] cipherPassword;
+//        private Stage currentStage;
+//
+//        //Default Constructor
+//        public SecureSignIn()
+//        {
+//                plainPassword = null;
+//                key = null;
+//                cipherPassword = null;
+//        }
+
+	/**
+	 * Method to encrypt the password
+	 * Based on Viginere's Cipher Algorithm, modified by Zander
+	 *
+	 * @param userPassword the password to be encrypted
+	 * @param key          the key used to encrypt the password
+	 * @return the encrypted password
+	 */
+	public static char[] encrypt(char[] userCipher, char[] k, int limit)
+	{
+		char[] systemCipher = new char[userCipher.length + 1];
+		char[] finalCipher = new char[userCipher.length * 2 + 1];
+		int keyIndex = 0;
+		int i = 0;
+		int ii = 0;
+		int temp;
+		int specCharCount = 0;
+		int pos = 0;
+		char[] specChars = new char[userCipher.length + 1];
+
+		for (char t : userCipher) {
+			if (t >= 65 && t <= 90) { //Encrypting Uppercase Characters
+				temp = t - 65 + (k[keyIndex] - 65);
+				if (temp < 0)
+					temp += 26;
+				if (temp <= 0)
+					temp += 26;
+
+				systemCipher[i++] = (char) (65 + (temp % 26));
+				if (++keyIndex == k.length)
+					keyIndex = 0;
+			} else if (t >= 97 && t <= 122) { //Encrypting Lower Case Characters
+				temp = t - 97 + (k[keyIndex] - 97);
+				if (temp < 0)
+					temp += 26;
+				if (temp < 0)
+					temp += 26;
+
+				systemCipher[i++] = (char) (97 + (temp % 26));
+				if (++keyIndex == k.length)
+					keyIndex = 0;
+			} else { //Encrypting Special Characters
+				specChars[ii++] = (char) (pos + 65);
+				specChars[ii++] = t;
+				specCharCount++;
+			}
+			pos++;
+		}
+		i = 0;
+		finalCipher[i++] = (char) (specCharCount == 0 ? 65 : (--specCharCount + 65));//Encrypting Amount of Special Characters in Password
+		for (char t = specChars[0]; t != 0; i++, t = specChars[i - 1])//Encrypting Special Characters & Positions of Special Characters
+			finalCipher[i] = t;
+		ii = i;
+		for (char t = systemCipher[0]; t != 0; i++, t = systemCipher[i - ii])//Encrypting Password
+			finalCipher[i] = t;
+
+		int ext = -1;
+		if (i > 32) {
+			while (true) {
+				System.out.println("Warning: Password is longer than 32 characters");
+				Console console = System.console();
+				String input = console.readLine("Would you like to shorten the pasword to the 32 limit? [ y | n ]\n");
+				if (input.toLowerCase().charAt(0) == 'y') {
+					ext = 1;
+					break;
+				} else if (input.toLowerCase().charAt(0) == 'n')
+					break;
+			}
+		}
+
+		int length = 0;
+		for (int x = 0; finalCipher[x] != '\0'; ++x)
+			length++;
+		char[] cipher = new char[length];
+		for (int xi = 0; xi < cipher.length && xi < length; ++xi)
+			cipher[xi] = finalCipher[xi];
+
+		//Shuffle Password
+		LinkedList<Character> evens = new LinkedList<>();
+		LinkedList<Character>odds = new LinkedList<>();
+		for (int iii = 0; iii < cipher.length; ++iii)
+			if ((int) cipher[iii] % 2 == 0)
+				evens.addLast(cipher[iii]);
+			else
+				odds.addFirst(cipher[iii]);
+		int iv = 0;
+		while (!evens.isEmpty() || !odds.isEmpty()) {
+			if (!odds.isEmpty()) {
+				cipher[iv++] = odds.getFirst();
+				odds.removeFirst();
+			}
+			if (!evens.isEmpty()) {
+				cipher[iv++] = evens.getFirst();
+				evens.removeFirst();
+			}
+		}
+
+		//encrypt special chars further
+		for (int v = 0; v < cipher.length; ++v)
+			if ((int) cipher[v] <= 47)
+				cipher[v] += 10;
+			else if ((int) cipher[v] > 47 && (int) cipher[v] < 64)
+				cipher[v] -= 5;
+			else if ((int) cipher[v] > 90 && (int) cipher[v] <= 96) {
+				if (cipher.length % 2 == 0)
+					cipher[v] += 2;
+				else
+					cipher[v] -= 2;
+			}
+
+		//Replacing unloved characters
+		for (int vi = 0; vi < cipher.length; ++vi)
+			if ((int) cipher[vi] == 34)
+				cipher[vi] = 123;
+			else if ((int) cipher[vi] == 38)
+				cipher[vi] = 124;
+			else if ((int) cipher[vi] == 60)
+				cipher[vi] = 125;
+			else if ((int) cipher[vi] == 62)
+				cipher[vi] = 126;
+
+		//Limitations
+		if (ext == 1 || limit < 32)
+		{
+			char[] cipherLimited = new char[limit < cipher.length ? limit : cipher.length];
+			for (int vii = 0; vii < cipher.length && vii < limit; ++vii)
+				cipherLimited[vii] = cipher[vii];
+			return cipherLimited;
+		}
+
+		return cipher;
+		//TODO: Remove null characters
+	}
+}
